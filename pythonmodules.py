7f03c84696f811e4064c335ac27fb4db404f7f3b
abhinave osho <75322268+feliz20020@users.noreply.github.com>
2024-09-23 14:24:01 +0530

    Add files via upload

---
+++ pythonmodules.py
@@ -0,0 +1,20 @@
+import math
+import  math as m
+example_variable=m.sqrt(25)
+power_variable=m.pow(2,5)
+ceil_variable=m.ceil(6.3)
+floor_variable=m.floor(5.5)
+fabs_variable=m.fabs(-9)
+factorial_variable=m.factorial(4)
+sin_variable=m.sin(m.pi/2)
+cos_variable=m.cos(m.pi/2)
+tan_variable=m.tan(m.pi/2)
+log2_variable=m.log2(3)
+log10_variable=m.log10(3)
+degree_variable=m.degrees(m.pi/2)
+radian_variable=m.radians(90)
+euler_variable=m.e
+nan_variable=m.nan
+gcd_variable=m.gcd()
+lcm_variable=m.lcm()
+print(nan_variable)
